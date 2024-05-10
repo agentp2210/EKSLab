70bdf8c773d20e57029012c92c4e21632ccff45a
An Tran <atra@gmail.com>
2024-05-10 20:38:17 +0200

    update eks manifest

--- scripts/tf-deploy-infra.sh
+++ scripts/tf-deploy-infra.sh
@@ -3,4 +3,10 @@ echo $(dirname "$0")
 
 cd ../terraform
 
-terraform plan -out aks.tfplan
+terraform plan -out eks.tfplan
+terraform apply eks.tfplan
+
+region=us-east-1
+cluster_name=$(terraform output -raw cluster_name)
+
+aws eks update-kubeconfig --region $region --name $cluster_name
--- terraform/eks.tf
+++ terraform/eks.tf
@@ -16,7 +16,15 @@ module "eks" {
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
+
+  enable_cluster_creator_admin_permissions = true
   cluster_endpoint_public_access = true
+
+  cluster_addons = {
+    aws-ebs-csi-driver = {
+      service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+    }
+  }
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
@@ -54,23 +62,12 @@ data "aws_iam_policy" "ebs_csi_policy" {
 
 module "irsa-ebs-csi" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version = "4.7.0"
+  version = "5.39.0"
 
   create_role                   = true
   role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
   provider_url                  = module.eks.oidc_provider
   role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-}
-
-resource "aws_eks_addon" "ebs-csi" {
-  cluster_name             = module.eks.cluster_name
-  addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.30.0-eksbuild.1"
-  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
-  tags = {
-    "eks_addon" = "ebs-csi"
-    "terraform" = "true"
-  }
 }
 
