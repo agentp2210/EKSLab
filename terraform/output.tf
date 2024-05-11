6db0f99c895ccb45dbab57bc2744a81252efe4ee
An Tran <atra@gmail.com>
2024-05-11 10:31:10 +0200

    add ecr

--- terraform/output.tf
+++ terraform/output.tf
@@ -34,6 +34,6 @@ output "cluster_name" {
   value       = module.eks.cluster_name
 }
 
-output "eks_managed_node_groups" {
-  value = module.eks.eks_managed_node_groups
+output "repository_url" {
+  value = module.eks.repository_url
 }
